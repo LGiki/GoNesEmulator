9f131b7e23a5e2c47f5b2d41201235cde49a05f2
LGiki <qq547221432@gmail.com>
2020-05-30 13:50:09 +0800

    Add Reset method for CPU registers

--- nes/cpu/registers.go
+++ nes/cpu/registers.go
@@ -14,12 +14,20 @@ const (
 	P_FLAG_CARRY        ProcessorStatus = 1 << iota // Carry flag
 	P_FLAG_ZERO                                     // Zero flag
 	P_FLAG_INTERUPT                                 // Interrupt disable flag
-	P_FLAG_DECIMAL_MODE                             // Decimal mode flag
+	P_FLAG_DECIMAL_MODE                             // Decimal mode flag. On the NES, this flag has no effect.
 	P_FLAG_BREAK                                    // Break flag
-	P_FLAG_UNUSED                                   // unused flag, alway 1
+	P_FLAG_UNUSED                                   // Unused flag, always 1
 	P_FLAG_OVERFLOW                                 // Overflow flag
 	P_FLAG_NEGATIVE                                 // Negative flag
 )
+
+func (r *Registers) Reset() {
+	r.P = 0x34
+	r.A = 0
+	r.X = 0
+	r.Y = 0
+	r.SP = 0xfd
+}
 
 func (p *ProcessorStatus) Set(flag ProcessorStatus, value bool) {
 	if value {
